ed76fd75acae48e7db9e78a6c1cf4f2e1ce12ffc
aliefian9 <58259558+aliefian9@users.noreply.github.com>
2021-03-10 10:48:17 +0700

    Update settings.py

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'rest_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ta_parkir',
+        'USER': 'aliefian',
+        'PASSWORD': '12345678',
+        'HOST': 'https://www.db4free.net/phpMyAdmin/',
+        'PORT': '3306',
     }
 }
 
